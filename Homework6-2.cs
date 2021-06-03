f6a70543c788ff6c8a1f68dd0c6e584b08ff2471
Pimrada641 <pimrada641@hotmail.com>
2021-06-03 22:42:34 +0700

    added file / edited name

---
+++ Homework6-2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("This number's prime factor is : ");
+            int PrimeFactor = 2;
+            while (num != 1)
+            {
+                while(num%PrimeFactor == 0)
+                {
+                    Console.Write("{0}, ",PrimeFactor);
+                    num = num / PrimeFactor;
+                }
+                PrimeFactor += 1;
+
+            }
+
+        }
+    }
+}
